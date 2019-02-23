23746e358092139bae481362126dc5ae098f9822
Nat Welch <nat@natwelch.com>
2019-02-23 18:38:31 +0000

    cleanup

--- components/Year.js
+++ components/Year.js
@@ -1,22 +1,16 @@
 const Year = ({ release, img, year, changes }) => {
+  let change_data = changes.map(change => {
+    return <li>{{ change }}</li>;
+  });
 
-  let change_data = changes.map((change) => {
-            return (
-              <li>{{change}}</li>
-            )
-          })
+  return (
+    <section className="mw8 center">
+      <a href={{ img }}>
+        <img src={{ img }} />
+      </a>
 
-
-    return (
-      <section className="mw8 center">
-        <a href={{img}}>
-          <img src={{img}} />
-        </a>
-
-        <h2>{{release}}</h2>
-        <ul>
-          {{change_data}}
-      </ul>
-      </section>
-    );
+      <h2>{{ release }}</h2>
+      <ul>{{ change_data }}</ul>
+    </section>
+  );
 };
