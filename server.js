8ea3d7297889fddce29c877105b3d0e82026a6b6
Nat Welch <nat@natwelch.com>
2019-02-23 20:19:40 +0000

    lint

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-const express = require('express')
-const next = require('next')
-const helmet = require('helmet')
-const compression = require('compression')
+const express = require("express");
+const next = require("next");
+const helmet = require("helmet");
+const compression = require("compression");
 
-const port = parseInt(process.env.PORT, 10) || 8080
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = parseInt(process.env.PORT, 10) || 8080;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = express()
-  server.use(compression())
+  const server = express();
+  server.use(compression());
 
-  server.get('/healthz', (req, res) => {
-    return "ok"
-  })
+  server.get("/healthz", (req, res) => {
+    return "ok";
+  });
 
-  server.get('*', (req, res) => {
-    return handle(req, res)
-  })
+  server.get("*", (req, res) => {
+    return handle(req, res);
+  });
 
   server.listen(port, err => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
