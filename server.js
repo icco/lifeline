1631dc651d2b8620afa92eb589c664e5b33bccb7
Nat Welch <nat@natwelch.com>
2019-02-23 20:10:42 +0000

    custom server

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const next = require('next')
+const helmet = require('helmet')
+const compression = require('compression')
+
+const port = parseInt(process.env.PORT, 10) || 8080
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  const server = express()
+  server.use(compression())
+
+  server.get('/healthz', (req, res) => {
+    return "ok"
+  })
+
+  server.use(helmet())
+
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(port, err => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
