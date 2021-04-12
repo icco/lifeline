d9ed56318b0ce178fa10851f590c997af2bf9577
Nat Welch <nat@natwelch.com>
2021-04-12 17:13:55 +0000

    lint

--- pages/_app.js
+++ pages/_app.js
@@ -2,5 +2,5 @@ import "tachyons.css";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
